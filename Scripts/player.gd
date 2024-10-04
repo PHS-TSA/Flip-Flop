f1039f25b13ea00eb358ba432b5846f1533f1782
iiAzui <rhys.harden@gmail.com>
2024-10-04 13:20:30 -0500

    jump right

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 signal pressed
 
 @export var speed = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -600.0
 
 var wasdOrArrows = true #true = wasd false = arrows
 var pause = false
