2c7d0d8cdf02071ebb4b3c76ff1f4368c53fdc27
iiAzui <rhys.harden@gmail.com>
2024-10-11 13:20:01 -0500

    finished the level

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -8,6 +8,7 @@ var wasdOrArrows = true #true = wasd false = arrows
 var pause = false
 var grabbing = false
 var walljump = false
+var crouching
 
 func _input(event):
 	if wasdOrArrows:
@@ -23,6 +24,28 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 	if not is_on_floor() and grabbing and not pause :
 		velocity += get_gravity() * delta/2
+		
+	#if crouching:
+		#%StandingSprite.visible = false
+		#%StandingCollison.disabled = true
+		#%StandingArea.monitoring = false
+		#%StandingArea.monitorable = false
+		#
+		#%DownSprite.visible = true
+		#%DownCollison.disabled = false
+		#%DownArea.monitoring = true
+		#%DownArea.monitorable = true
+	#else:
+		#%StandingSprite.visible = true
+		#%StandingCollison.disabled = false
+		#%StandingArea.monitoring = true
+		#%StandingArea.monitorable = true
+		#
+		#%DownSprite.visible = false
+		#%DownCollison.disabled = true
+		#%DownArea.monitoring = false
+		#%DownArea.monitorable = false
+		
 	if not pause:
 		if wasdOrArrows:
 			if Input.is_action_just_pressed("w") and (is_on_floor() or grabbing):
@@ -40,11 +63,15 @@ func _physics_process(delta: float) -> void:
 			elif not walljump:
 				velocity.x = move_toward(velocity.x, 0, speed/3)
 			
-			if is_on_wall_only() and Input.is_action_pressed("wasdgrab"):
+			if is_on_wall() and Input.is_action_pressed("wasdgrab"):
 				grabbing = true
 			else:
 				grabbing = false
 			
+			#if Input.is_action_pressed("down"):
+				#crouching = true
+			#else:
+				#crouching = false
 				
 		else:
 			if Input.is_action_just_pressed("up") and (is_on_floor() or grabbing):
@@ -65,11 +92,17 @@ func _physics_process(delta: float) -> void:
 			elif not walljump:
 				velocity.x = move_toward(velocity.x, 0, speed/3)
 				
-			if is_on_wall_only() and Input.is_action_pressed("arrowsgrab"):
+			if is_on_wall() and Input.is_action_pressed("arrowsgrab"):
 				grabbing = true
 			else:
 				grabbing = false
-
+			
+			#if Input.is_action_pressed("s"):
+				#crouching = true
+			#else:
+				#crouching = false
+				
+				
 		move_and_slide()
 		
 func _on_area_2d_area_entered(area: Area2D) -> void:
