bf16c2d4ac8e4218dc59d1c6d126ad80087a6300
iiAzui <rhys.harden@gmail.com>
2025-02-17 11:10:04 -0600

    teleports

---
+++ area_2d.gd
@@ -0,0 +1,13 @@
+extends Area2D
+@export var level = ""
+
+
+func _on_area_entered(area: Area2D) -> void:
+	get_tree().change_scene_to_file.call_deferred(level)
+	print("entered")
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		get_tree().change_scene_to_file.call_deferred(level)
+		print("entered")
