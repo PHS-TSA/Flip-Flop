e82b84d31017cf86c8f0faa687700edd235bbb60
iiAzui <rhys.harden@gmail.com>
2025-03-24 10:21:00 -0500

    springs ingleplater

--- Scripts/spring.gd
+++ Scripts/spring.gd
@@ -6,20 +6,33 @@ var bodi
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if player == true:
-		if Input.is_action_pressed("wasdgrab"):
-			if playerin:
-				bodi.velocity.y = -1800
-			%pre.visible = false
-			$Up.visible = true
-			await get_tree().create_timer(0.1).timeout
-			get_node("Up/StaticBody2D/CollisionShape2D").disabled = false 
+	if Globals.singleplayer == false:
+		if player == true:
+			if Input.is_action_pressed("wasdgrab"):
+				if playerin:
+					bodi.velocity.y = -1800
+				%pre.visible = false
+				$Up.visible = true
+				await get_tree().create_timer(0.1).timeout
+				get_node("Up/StaticBody2D/CollisionShape2D").disabled = false 
+			else:
+				%pre.visible = true
+				$Up.visible = false
+				get_node("Up/StaticBody2D/CollisionShape2D").disabled = true 
 		else:
-			%pre.visible = true
-			$Up.visible = false
-			get_node("Up/StaticBody2D/CollisionShape2D").disabled = true 
+			if Input.is_action_pressed("arrowsgrab"):
+				if playerin:
+					bodi.velocity.y = -1800
+				%pre.visible = false
+				$Up.visible = true
+				await get_tree().create_timer(0.1).timeout
+				get_node("Up/StaticBody2D/CollisionShape2D").disabled = false 
+			else:
+				%pre.visible = true
+				$Up.visible = false
+				get_node("Up/StaticBody2D/CollisionShape2D").disabled = true 
 	else:
-		if Input.is_action_pressed("arrowsgrab"):
+		if Input.is_action_pressed("wasdgrab"):
 			if playerin:
 				bodi.velocity.y = -1800
 			%pre.visible = false
