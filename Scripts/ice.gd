8fcd6a26a5c24ea1717b601dd60b15d17ff57fe0
iiAzui <rhys.harden@gmail.com>
2025-04-11 13:34:56 -0500

    bleg

--- Scripts/ice.gd
+++ Scripts/ice.gd
@@ -1,9 +1,8 @@
 extends Sprite2D
 
 func _process(delta: float) -> void:
-	if get_node("Area2D").has_overlapping_bodies():
-		if get_node("Area2D").get_overlapping_bodies().any(func bleg(body): return body.name == "Player"):
-			Globals.onice = true
+	if get_node("Area2D").has_overlapping_bodies() and get_node("Area2D").get_overlapping_bodies().any(func bleg(body): return body.name == "Player"):
+		Globals.onice = true
 	else:
 		Globals.onice = false
 		print(Globals.onice)
